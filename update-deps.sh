d792af11a1a717392811171692b47c8f361b489c
Tulip Blossom <tulilirockz@outlook.com>
2026-06-03 16:25:35 -0300

    chore: build vanilla extractor from source + minor cleanups (#8)
    * chore: build vanilla extractor from source + minor cleanups
    
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>
    
    * Update the extractor
    
    ---------
    
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>
    Co-authored-by: biroder <107300789+biroder@users.noreply.github.com>

--- update-deps.sh
+++ update-deps.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -xeuo pipefail
 
-TARGET_VERSION="${1:-0.102.0-beta7}"
+TARGET_DOUKUTSU_VERSION="${1:-0.102.0-beta7}"
+TARGET_EXTRACTOR_VERSION="${1:-1.0.1}"
 
-LOCK_FILE_DIR=$(mktemp -d)
+DOUKUTSU_LOCK_FILE_DIR=$(mktemp -d)
+EXTRACTOR_LOCK_FILE_DIR=$(mktemp -d)
 cleanup() {
-  rm -r "$LOCK_FILE_DIR"
+  rm -r "$DOUKUTSU_LOCK_FILE_DIR"
+  rm -r "$EXTRACTOR_LOCK_FILE_DIR"
 }
+
 trap cleanup EXIT
-set -x
-curl -s -L "https://github.com/doukutsu-rs/doukutsu-rs/archive/refs/tags/$TARGET_VERSION.tar.gz" | tar xzf - -C "$LOCK_FILE_DIR"
+
+curl -fsSL "https://github.com/doukutsu-rs/doukutsu-rs/archive/refs/tags/${TARGET_DOUKUTSU_VERSION}.tar.gz" | tar xzf - -C "$DOUKUTSU_LOCK_FILE_DIR"
 podman run --rm -it \
   -v .:/tmp/build:Z \
-  -v "$LOCK_FILE_DIR:$LOCK_FILE_DIR:Z" \
+  -v "$DOUKUTSU_LOCK_FILE_DIR:$DOUKUTSU_LOCK_FILE_DIR:Z" \
   --pull newer \
   docker.io/library/python:latest \
-  sh -c "pip install aiohttp toml && /tmp/build/flatpak-builder-tools/cargo/flatpak-cargo-generator.py ${LOCK_FILE_DIR}/doukutsu-rs-${TARGET_VERSION}/Cargo.lock -o /tmp/build/cargo-sources.json"
+  sh -c "pip install aiohttp toml && /tmp/build/flatpak-builder-tools/cargo/flatpak-cargo-generator.py ${DOUKUTSU_LOCK_FILE_DIR}/doukutsu-rs-${TARGET_DOUKUTSU_VERSION}/Cargo.lock -o /tmp/build/cargo-sources-doukutsu.json"
+
+curl -fsSL "https://github.com/doukutsu-rs/vanilla-extractor/archive/refs/tags/v${TARGET_EXTRACTOR_VERSION}.tar.gz" | tar xzf - -C "$EXTRACTOR_LOCK_FILE_DIR"
+podman run --rm -it \
+  -v .:/tmp/build:Z \
+  -v "$EXTRACTOR_LOCK_FILE_DIR:$EXTRACTOR_LOCK_FILE_DIR:Z" \
+  --pull newer \
+  docker.io/library/python:latest \
+  sh -c "pip install aiohttp toml && /tmp/build/flatpak-builder-tools/cargo/flatpak-cargo-generator.py ${EXTRACTOR_LOCK_FILE_DIR}/vanilla-extractor-${TARGET_EXTRACTOR_VERSION}/Cargo.lock -o /tmp/build/cargo-sources-vanilla-extractor.json"
